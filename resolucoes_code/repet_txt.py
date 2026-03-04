3ee8c75a3b71c051557a6b72674f56981eadd08e
Vítor Fontenele <132603905+fontenelevitor@users.noreply.github.com>
2026-03-04 17:06:44 -0300

    Exercicio 2

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,7 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+info1 = input("Digite a primeira info: ")
+info2 = input("Digite a segunga info: ")
+
+info_concatena = info1 + " "+ info2
+
+print("As informações concatenadas são: ",info_concatena)
