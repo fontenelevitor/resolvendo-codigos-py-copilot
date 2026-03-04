6c0062d8c178b58008957eff6a2bc80b12670472
Vítor Fontenele <132603905+fontenelevitor@users.noreply.github.com>
2026-03-04 17:16:41 -0300

    Exercicio 3

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,16 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+operador = input("Digite o operador (+, -, *, /): ")
+
+if operador == "+":
+    print(num1 + num2)
+elif operador == "-":
+    print(num1 - num2)
+elif operador == "*":
+    print(num1 * num2)
+elif operador == "/":
+    print(num1 / num2)
+else:
+    print("Operador inválido")
